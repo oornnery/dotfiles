e423676e4eec5580c32648d3c4933bf801902e4b
Fabio Souza <fabiohcsouza@outlook.com.br>
2023-03-30 19:22:20 -0300

    new file:   .config/gtk-3.0/old-settings.ini 	modified:   .config/i3/config 	new file:   .config/polybar/old-config.ini 	modified:   backup.sh 	new file:   export_config.sh

---
+++ export_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cp -r .config/dunst ~/.config
+cp -r .config/gtk-3.0 ~/.config
+cp -r .config/htop ~/.config
+cp -r .config/hypr ~/.config
+cp -r .config/i3 ~/.config
+cp -r .config/neofetch ~/.config
+cp -r .config/polybar ~/.config
+cp -r .config/redshift ~/.config
+cp -r .config/scripts ~/.config
