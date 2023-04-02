055ef30f75546a8a86703446a929d3755248f6f7
Fabio Souza <fabiohcsouza@outlook.com.br>
2023-04-02 15:51:38 -0300

    modified:   .config/scripts/extender_monitor.sh 	modified:   packages.toml

--- .config/scripts/extender_monitor.sh
+++ .config/scripts/extender_monitor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-xrandr --output LVDS-1 --auto --pos 0x0 --output VGA-1 --auto --primary --pos 1366x0
+xrandr --output LVDS1 --auto --pos 0x0 --output VGA1 --auto --primary --pos 1366x0
 
