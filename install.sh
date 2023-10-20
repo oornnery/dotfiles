4b1c519fb18c351893ae1458a1d02127f974b90c
Fabio Souza <fabiohcsouza@outlook.com.br>
2023-10-20 12:46:43 -0300

    ''

--- install.sh
+++ install.sh
@@ -7,6 +7,27 @@ packages_extras=$(cat dotfiles/packages_extras.txt)
 packages_scripts=$(cat dotfiles/packages_scripts.txt)
 user=$(whoami)
 
+# Fazer backup
+# Copiar arquivos de configuração
+cp $HOME/.bashrc $HOME/.bashrc.bak
+cp $HOME/.bash_history $HOME/.bash_history.bak
+cp $HOME/.zshrc $HOME/.zshrc.bak
+cp $HOME/.zsh_history $HOME/.zsh_history.bak
+cp $HOME/.p10k.zsh $HOME/.p10k.zsh.bak
+cp $HOME/.vimrc $HOME/.vimrc.bak
+
+# Compiar diretorio de configuração
+cp -r $HOME/.config $HOME/.config.bak
+cp -r $HOME/.oh-my-zsh $HOME/.oh-my-zsh.bak
+cp -r $HOME/.wallpaper $HOME/.wallpaper.bak
+cp -r $HOME/.scripts $HOME/.scripts.bak
+cp -r $HOME/.screenlayout $HOME/.screenlayout.bak
+
+
+# Instalar pacotes
+# Copiar arquivos
+# Atualizar
+
 
 
 # Atualizando sistema
