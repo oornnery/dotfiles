d194641b48de95280b2204c82908cb3952cc5469
Fabio Souza <fabiohcsouza@outlook.com.br>
2024-02-21 22:36:46 -0300

    new config hypr

--- dotfiles/.config/qtile/autostart.sh
+++ dotfiles/.config/qtile/autostart.sh
@@ -3,18 +3,31 @@
 
 ### Set wallpaper
 # feh --bg-fill ~/.wallpaper/archlinux_1.jpg
+feh $HOME/Wallpaper/Aesthetic2.png -F --bg-fill &
 
 ### Set screen layout ###
-# ~/.screenlayout/layout.sh &
+#~/.screenlayout/layout_edp_hdmi-a-0.sh &
+
+num_monitors=$(xrandr --listmonitors | grep -oP "Monitors: \K\d+")
+
+# Verifica o numero de monitores
+if [ $num_monitors -eq 1]; then
+  autorandr default
+elif [ $num_monitors -eq 2 ]; then
+  autorandr default_hdmi
+else
+  autorandr default
+fi
+
 
 # Picom compositor
 picom -b &
 
 # Auth with polkit-kde-agent:
-/usr/lib/polkit-kde-authentication-agent-1 &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
 # Desktop notifications
-mako &
+dunst &
 
 # Network Applet
 nm-applet --indicator &
