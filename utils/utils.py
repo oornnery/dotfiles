b218c2653dbaf3cfb8317875ed518e9d9441ca7f
Fabio Souza <fabiohcsouza@outlook.com.br>
2024-03-16 23:18:30 -0300

    termiando install

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,7 @@
 import subprocess
+import shutil
+import logging
+from pathlib import Path
 
 def install_packages(packages: list[str], inter: str = "pacman"):
     """
@@ -30,15 +33,56 @@ def install_packages(packages: list[str], inter: str = "pacman"):
     __run_command__(command)
     
 
-def backup(orignal_path: str, destination_path: str = "./dotfiles/backup", action: str = "copy"):
-    """
-    Backup a directory by either copying or moving it to a specified destination.
-    
-    Args:
-        orignal_path (str): The path of the directory to be backed up.
-        destination_path (str): The path where the directory should be backed up to. Defaults to "./dotfiles/backup".
-        action (str): The action to perform for the backup. Options are "copy" or "move". Defaults to "copy".
-    """
-    _orignal_path = orignal_path
-    _destination_path = destination_path
-    _action = action
+
+def configurar_logger():
+    # Configura o logger
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s',
+                        filename='copiar_mover.log',
+                        filemode='a')
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+
+
+log = logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s',
+                        filename='copiar_mover.log',
+                        filemode='a')
+def copiar_e_mover(file: str, origin_path: str, destination_path: str, backup: bool = True):
+    origin = Path(origin_path)
+    destination = Path(destination_path)
+    backup = Path('.bak')
+
+    try:
+        file_original_path = Path(destination_path+file)
+        # Execulta o backup do arquivo original
+        if file_original_path.exists() and backup:
+            shutil.move(file_original_path, backup)
+            log.info(f"File '{file_original_path.name}' moved from '{origin}' to '{backup}'.")
+        
+        # Copia o arquivo para o destino
+        shutil.copy2(origin, destination)
+        log.info(f"Arquivo copiado de '{origin}' para '{destination}'.")
+
+        log.info(f"Arquivo original movido de '{origin}' para '{backup}'.")
+    except FileNotFoundError:
+        log.error(f"Erro: O arquivo de origem '{origin}' não existe.")
+        return False
+    except Exception as e:
+        log.error(f"Erro: {e}")
+        return False
+
+    return True
+
+if __name__ == "__main__":
+    configurar_logger()
+
+    # Exemplo de uso
+    origem = "/caminho/para/arquivo.txt"
+    destino = "/caminho/para/destino/arquivo.txt"
+    backup = "/caminho/para/backup"
+
+    copiar_e_mover(origem=origem, destino=destino, backup=backup)
