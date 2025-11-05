ceff05489540725523cd90c83c57fa364691754c
Fabio Souza <fabiohcsouza@outlook.com.br>
2025-11-05 15:35:27 -0300

    fix script py

--- dots/linux/scripts/python.sh
+++ dots/linux/scripts/python.sh
@@ -6,8 +6,12 @@ sudo apt install nala
 sudo nala update
 
 sudo nala install -y \
-    make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
-    libsqlite3-dev libncurses-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
-    libffi-dev liblzma-dev curl git
+  make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev \
+  libsqlite3-dev libncurses-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
+  libffi-dev liblzma-dev curl git
 
 sudo nala install -y python3 python3-pip python3-venv python3-neovim
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+cd ~/.pyenv && src/configure && make -C src
+
